7051837d1cc923ef5d822ccb3e4e44f02599d844
prateek44 <44beastmodeon@gmail.com>
2018-05-16 20:52:29 +0530

    minification proof di App

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-(function()
-{
-  'use strict';
-  angular.module("DIApp",[])
-  .controller("DIAppController",function($scope)
-{
-$scope.name="prateek";
-});
-})();
+// (function()
+// {
+//   'use strict';
+//   angular.module("DIApp",[])
+//   .controller("DIAppController",['$scope','$filter',DIAppController]);
+// function DIAppController($scope,$filter,$injector)
+// {
+//   console.log(DIAppController.toString());
+// $scope.name="prateek";
+// $scope.upper=function()
+// {
+//   var upCase=$filter('uppercase');
+//   $scope.name=upCase($scope.name);
+// };
+//
+// }
+//
+// })();
+!function(){"use strict";angular.module("DIApp",[]).controller("DIAppController",["$scope","$filter",function e(n,o,r){console.log(e.toString());n.name="prateek";n.upper=function(){var e=o("uppercase");n.name=e(n.name)}}])}();
