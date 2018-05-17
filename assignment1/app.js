5821f48d5f98ab55c98126777c9b1bf09fefd1fc
prateek44 <44beastmodeon@gmail.com>
2018-05-17 17:25:55 +0530

    assignment 1 coursera

--- assignment1/app.js
+++ assignment1/app.js
@@ -5,9 +5,32 @@
   .controller("FoodCounterAppController",['$scope','$filter',FoodCounterAppController]);
   function FoodCounterAppController($scope,$filter)
   {
+    console.log(FoodCounterAppController.toString());
     $scope.items="prateek";
+    $scope.foodItemsCount='0';
+    $scope.message='';
     $scope.checkNumberOfItems=function()
     {
+      var foodItems=$scope.items;
+      var comma=',';
+      var arrayOfFoodItems=foodItems.split(comma);
+      console.log(arrayOfFoodItems);
+      console.log('number of food items'+arrayOfFoodItems.length);
+      $scope.foodItemsCount=arrayOfFoodItems.length;
+      if($scope.foodItemsCount<=3)
+      {
+
+        $scope.message='enjoy'
+        console.log('enjoy');
+      }
+      else
+      {
+        $scope.message='too much!'
+        console.log('too much!');
+
+      }
+
+
 
     };
 
